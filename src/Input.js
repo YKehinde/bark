5b165707afe8c944afc93d37e4b356e68cd9c774
Yemi <y.kehinde@hotmail.com>
2021-04-02 18:37:43 +0100

    Updated the input component onChange event

--- src/Input.js
+++ src/Input.js
@@ -16,7 +16,7 @@ const Input = (
   return (
     <div className="form-group">
       <label htmlFor={forInput}>{label}</label>
-      <input type={type} name={name} id={id} placeholder={placeholder} className={`form-control ${classList ? classList:''}`} onChangeValue={handleChangeValue}/>
+      <input type={type} name={name} id={id} placeholder={placeholder} className={`form-control ${classList ? classList:''}`} onChange={handleChangeValue}/>
       {smallText ? 
       <small id={descriptionId} className="form-text text-muted">{smallText}</small> :
       null
